796d6252dea05440e4ed373908690c56a0d489cc
“Mateus <devmateussnr@gmail.com>
2023-06-18 17:55:59 -0300

    Init push

---
+++ Case1/ExtensionMethods/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        public static void Main()
+        {
+            DateTime dt = new DateTime(2023, 1, 16, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+        }
+    }
+}
---
+++ Case2/Case2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ExtensionMethods
+{
+    class Progrma
+    {
+        public static void Main()
+        {
+            string s1 = "Good morning dear students!";
+            Console.WriteLine(s1.Cut(10));
+        }
+    }
+}
